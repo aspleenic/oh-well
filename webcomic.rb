74a9ceab3b4155d341d6eb2859307f37fcd8b0e3
aspleenic <pjhagerty@gmail.com>
2023-10-27 09:25:06 -0400

    Ensuring the latest code is in github

--- webcomic.rb
+++ webcomic.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 # Define the comic chapters with their respective pages
 COMIC_CHAPTERS = [
-  { chapter: '1', folder: 'chapter1', pages: ['OWL001-001.jpg', 'OWL001-002.jpg', 'OWL001-003.jpg', 'OWL001-004.jpg', 'OWL001-005.jpg', 'OWL001-006.jpg', 'OWL001-007.jpg', 'OWL001-008.jpg', 'OWL001-009.jpg', 'OWL001-010.jpg', 'OWL001-011.jpg', 'OWL001-012.jpg', 'OWL001-013.jpg', 'OWL001-014.jpg'], cover: 'OWL001-cover.jpg' },
+  { chapter: '1', folder: 'chapter1', pages: ['OWL001-001.jpg', 'OWL001-002.jpg', 'OWL001-003.jpg', 'OWL001-004.jpg', 'OWL001-005.jpg', 'OWL001-006.jpg', 'OWL001-007.jpg', 'OWL001-008.jpg', 'OWL001-009.jpg', 'OWL001-010.jpg', 'OWL001-011.jpg', 'OWL001-012.jpg'], cover: 'OWL001-cover.jpg' },
   { chapter: '2', folder: 'chapter2', pages: ['OWL002-001.jpg', 'OWL002-002.jpg'], cover: 'OWL002-001.jpg' }
   # Add more chapters here...
 ]
@@ -39,13 +39,6 @@ get '/' do
   next_chapter = current_chapter + 1
 
   erb :index, locals: {
-    # latest_page: latest_page,
-#     current_chapter: current_chapter,
-#     current_page: current_page,
-#     prev_page: prev_page,
-#     next_page: next_page,
-#     prev_chapter: prev_chapter,
-#     next_chapter: next_chapter
     latest_page: latest_chapter[:pages][current_page - 1],
     current_chapter: current_chapter,
     current_page: current_page,
